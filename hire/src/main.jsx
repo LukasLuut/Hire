cc1f784b8260ccdcfad6764b5b84a957cc7bfc93
leandrafernanda <leandrafriedrich@gmail.com>
2025-10-07 16:34:13 -0300

    adicionado o pagamento

--- hire/src/main.jsx
+++ hire/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login'
+import Payment from './pages/Payment'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    {/* <Login/> */}
+    <Payment/>
   </StrictMode>,
 )
